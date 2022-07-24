8d4f3bab8da4feedd624282af8f66253292f2948
mbarbosasan <moisesbarbosa23@gmail.com>
2022-07-24 19:07:18 -0300

    add: photos upload'

--- src/main/java/io/github/mbarbosasan/agendaapi/controller/ContatoController.java
+++ src/main/java/io/github/mbarbosasan/agendaapi/controller/ContatoController.java
@@ -3,15 +3,20 @@ package io.github.mbarbosasan.agendaapi.controller;
 import io.github.mbarbosasan.agendaapi.model.entities.Contato;
 import io.github.mbarbosasan.agendaapi.model.repository.ContatoRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/contatos")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ContatoController {
 
     private final ContatoRepository repository;
@@ -33,12 +38,30 @@ public class ContatoController {
     }
 
     @PatchMapping("{id}/favorito")
-    public void favorite(@PathVariable Long id, @RequestBody Boolean favorito) {
+    public void favorite(@PathVariable Long id) {
         Optional<Contato> contato = repository.findById(id);
         contato.ifPresent(c -> {
-            c.setFavorito(favorito);
+            boolean favorito = c.getFavorito() == Boolean.TRUE;
+            c.setFavorito(!favorito);
             repository.save(c);
         });
+    }
+    @PutMapping("{id}/foto")
+    public byte[] addPhoto(@PathVariable Long id, @RequestParam("foto") Part arquivo) {
+       Optional<Contato> contato = repository.findById(id);
+       return contato.map( c -> {
+           try {
+               InputStream is = arquivo.getInputStream();
+               byte[] bytes = new byte[(int) arquivo.getSize()];
+               IOUtils.readFully(is, bytes);
+               c.setFoto(bytes);
+               repository.save((c));
+               is.close();
+               return bytes;
+           } catch (IOException e) {
+               return null;
+           }
+       }).orElse(null);
     }
 
 }
--- src/main/java/io/github/mbarbosasan/agendaapi/model/entities/Contato.java
+++ src/main/java/io/github/mbarbosasan/agendaapi/model/entities/Contato.java
@@ -21,6 +21,9 @@ public class Contato {
     private String email;
     @Column
     private Boolean favorito ;
+    @Column
+    @Lob
+    private byte[] foto;
 
 }
 
